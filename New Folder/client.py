75a4f4b9bb4fdc6d6e859112aacfa460564ad487
lokkelvin <kelvin.lok266@gmail.com>
2019-04-16 20:09:17 +0800

    new client

--- New Folder/client.py
+++ New Folder/client.py
@@ -26,29 +26,29 @@ def get_request(filename, persistent = True):
 'GET /c.txt HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nConnection: close\r\n\n'
 
 
-def main():
+def persistent():
     
     t1 = time.time()
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((ADDR,PORT))
-    #print "[client] Requesting a.jpg"
+    print "[client] Requesting a.jpg"
     s.send(b'GET /a.jpg HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nConnection: keep-alive\r\n\n')
     recv_file(s,'received.jpg')
-    #print "[client] Received a.jpg"
+    print "[client] Received a.jpg"
     
     t2 = time.time()
     
-    #print "[client] Requesting b.mp3"
+    print "[client] Requesting b.mp3"
     s.send(b'GET /b.mp3 HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nConnection: keep-alive\r\n\n')
     recv_file(s,'received.mp3')
-    #print "[client] Received b.mp3"
+    print "[client] Received b.mp3"
     
     t3 = time.time()
     
-    #print "[client] Requesting c.txt"
+    print "[client] Requesting c.txt"
     s.send(b'GET /c.txt HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nConnection: keep-alive\r\n\n')
     recv_file(s, 'received.txt')
-    #print "[client] Received c.txt"
+    print "[client] Received c.txt"
     
     s.close()
     t4 = time.time()
@@ -58,12 +58,46 @@ def main():
     print "b.mp3: {:.6f} ms".format((t3-t2)/10e-3)
     print "c.txt: {:.6f} ms".format((t4-t3)/10e-3)
     print "total: {:.6f} ms".format((t4-t1)/10e-3)
+ 
     
-  
-def echo(s):
-    s.sendall(b'Hello World\n')
-    data = s.recv(1024)
-    #print 'Received', repr(data)
+def nonpersistent():
+    
+    t1 = time.time()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ADDR,PORT))
+    print "[client] Requesting a.jpg"
+    s.send(b'GET /a.jpg HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nConnection: close\r\n\n')
+    recv_file(s,'received.jpg')
+    print "[client] Received a.jpg"
+    s.close()
+    
+    t2 = time.time()
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ADDR,PORT))
+    print "[client] Requesting b.mp3"
+    s.send(b'GET /b.mp3 HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nConnection: close\r\n\n')
+    recv_file(s,'received.mp3')
+    print "[client] Received b.mp3"
+    s.close()
+    
+    t3 = time.time()
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((ADDR,PORT))
+    print "[client] Requesting c.txt"
+    s.send(b'GET /c.txt HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nConnection: close\r\n\n')
+    recv_file(s, 'received.txt')
+    print "[client] Received c.txt"
+    s.close()
+    
+    t4 = time.time()
+
+    print "[client] Connection closed\n"
+    print "a.jpg: {:.6f} ms".format((t2-t1)/10e-3)
+    print "b.mp3: {:.6f} ms".format((t3-t2)/10e-3)
+    print "c.txt: {:.6f} ms".format((t4-t3)/10e-3)
+    print "total: {:.6f} ms".format((t4-t1)/10e-3)
 
 
 def recv_file(s,filename):
@@ -75,17 +109,29 @@ def recv_file(s,filename):
             if not data: break
             if endmarker in data: 
                 loc = data.find(endmarker)
-                ##print "End of file"
-                ##print "[{} with endmarker]".format(counter), repr(data)
-                ##print "[{} without endmarker]".format(counter), repr(data[:loc])
+                #print "End of file"
+                #print "[{} with endmarker]".format(counter), repr(data)
+                #print "[{} without endmarker]".format(counter), repr(data[:loc])
                 f.write(data[:loc])
                 break
+            s.send(b'ack') # dont ack the eof packet
             counter += 1
             #print "Received {} :".format(counter)#, repr(data)
             f.write(data)
     f.close()
+    print "Received {}, {} packets".format(filename, counter)
 
 
 if __name__ == '__main__':
-    print "Persistent"
-    main()
+    
+    if sys.argv[1] == '1':
+        print "Persistent"
+        persistent()
+    if sys.argv[1] == '2':
+        print "Non persistent"
+        nonpersistent()
+    
+    
+    
+    
+    
--- New Folder/server.py
+++ New Folder/server.py
@@ -18,148 +18,88 @@ import time
 ADDR = ''
 PORT = 12346
 
-activeChildren = [] #list of pid of active children
-def close_children():
-    '''os.waitpid(pid, options)
-
-Wait for completion of a child process given by process id pid, and return a tuple containing its process id and exit status indication (encoded as for wait()). The semantics of the call are affected by the value of the integer options, which should be 0 for normal operation.
-
-If pid is greater than 0, waitpid() requests status information for that specific process. If pid is 0, the request is for the status of any child in the process group of the current process. If pid is -1, the request pertains to any child of the current process. If pid is less than -1, status is requested for any process in the process group -pid (the absolute value of pid).
-
-An OSError is raised with the value of errno when the syscall returns -1.'''
-    while activeChildren:
-        try:
-            pid,status = os.waitpid(0, os.WNOHANG)
-        except:
-            #print "Failed to kill pid: {}".format(pid)
-            pass
-        if not pid: break
-        #print "pid: {} exited with status {}".format(pid,status)
-        activeChildren.remove(pid)
-
 
 def main():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.bind((ADDR,PORT))
     s.listen(5) # Backlog  = 5
-    #conn is a new socket object usable to send and receive data on the connection, and address is the address bound to the socket on the other end of the connection
-    # address = tuple of addr and port. Use *arg to unpack tuple.
     while(1):
         try:
-            close_children()
+            print '[main] Listening for connections . . .'
             conn, address = s.accept()
-            #print "[Parent] New client connected from IP address {} and port number {}".format(*address)
-            #print "[Parent {}] before forking".format(os.getpid())
-            pid = os.fork()
-            if pid == 0: # if children process
-                # handle request and close connection
-                #handle_client_echo(conn,address)
-                s.close() # close listen port in child
-                s = None
-                handle_client_file(conn,address)
-                break
-            else: # if parent process
-                # keep listening for next connection
-                conn.close() # Remove reference to connection/ Decrement reference count, otherwise child's connection will stay alive due to ghost reference
-                conn = None
-                #print "[Parent {}] New child pid = {}".format(os.getpid(),pid)
-                activeChildren.append(pid)
-                
+            print "[main] New client connected from IP address {} and port number {}".format(*address)
+            getrequest = conn.recv(1024).split()
+            filename = getrequest[1][1:]
+            connectiontype = getrequest[6]
+            if connectiontype == 'keep-alive':
+                print '[main] Client requests {}, Persistent'.format(filename)
+                handle_client_persistent(filename,conn)
+            else:
+                print '[main] Client requests {}, Non persistent'.format(filename)
+                handle_client_nonpersistent(filename,conn)
         except KeyboardInterrupt:
-            if conn:
-                conn.close()
-                #print "[process {}] conn closed".format(os.getpid())
+            if conn: conn.close()
+            if s: s.close()
             break
+        except:
+            if conn: conn.close()
+            if s: s.close()
+            raise
     if s:
         s.close()
-        #print "[parent {}] listen closed".format(os.getpid())
 
-            
-def handle_client_file(conn,address):
-    #print "[child {}] Closed listen".format(os.getpid())
-    #time.sleep(2) # simulated 2 second delay
-    
-    getrequest = conn.recv(1024).split() #e.g of getrequest is 'GET /a.jpg HTTP/1.1\r\nHost: 127.0.0.1:12345\r\nConnection: keep-alive\r\n\n'
-    filename = getrequest[1][1:] #e.g. 'a.jpg'
-    connectiontype = getrequest[6] #e.g. 'keep-alive' or 'closed'
-    if connectiontype == 'keep-alive2':
+def handle_client_persistent(filename, conn):
+    f = open(filename,'rb')
+    line = f.read(1024)
+    while line:
+        conn.send(line)
+        conn.recv(10)
+        line = f.read(1024)
+    conn.send(b'end_of_file')
+    f.close()
+    getrequest = conn.recv(1024)
+    while getrequest:
+        filename = getrequest.split()[1][1:]
         f = open(filename,'rb')
         line = f.read(1024)
         counter = 0
         while line:
             conn.send(line)
+            conn.recv(10)
             counter += 1
-            ##print "Sent {} ".format(counter), repr(line)
             line = f.read(1024)
         conn.send(b'end_of_file')
-        #print "Sent {}, {}, {}".format(counter,filename, connectiontype)
+        print "[persistent] Sent {}, {}".format(counter,filename)
         f.close()
-        #print "Done"
-        handle_client_file(conn,address) # recursive call - base case when connectiontype = closed
-        conn.close()
-        conn = None
-    elif connectiontype == 'keep-alive':
-        f = open('a.jpg','rb')
-        line = f.read(1024)
-        counter = 0
-        while line:
-            conn.send(line)
-            line = f.read(1024)
-        conn.send(b'end_of_file')
-        f.close()
-        x=conn.recv(1024)
-        f = open('b.mp3','rb')
-        line = f.read(1024)
-        counter = 0
-        while line:
-            conn.send(line)
-            line = f.read(1024)
-        conn.send(b'end_of_file')
-        f.close()
-        x=conn.recv(1024)
-        f = open('c.txt','rb')
-        line = f.read(1024)
-        counter = 0
-        while line:
-            conn.send(line)
-            line = f.read(1024)
-        conn.send(b'end_of_file')
-        f.close()
-    else:
-        f = open(filename,'rb')
-        line = f.read(1024)
-        counter = 0
-        while line:
-            conn.send(line)
-            counter += 1
-            ##print "Sent {} ".format(counter), repr(line)
-            line = f.read(1024)
-        conn.send(b'end_of_file')
-        #print "Sent {}, {}, {}".format(counter,filename, connectiontype)
-        f.close()
-        #print "Done"
-        conn.close() # close connection to let client know the transfer is complete
-        conn = None
-    #print "[child {}] Closed accept".format(os.getpid())
-    #os._exit(0)
-        
-def handle_client_echo(conn,address):
-    time.sleep(2) # simulated 2 second delay
-    while True:
-        data = conn.recv(1024)
-        if not data: break
-        #print "[Received {} bytes from {}:{}]".format(len(data), *address)
-        conn.send('[{}]: {}'.format(time_now(),data))
-        #print "[Sent message: {} to {}:{}]".format(data,*address)
-    #print "[Client {}:{} disconnected]".format(*address)
+        getrequest = conn.recv(1024)
+    print '[persistent] Client has disconnected (Peer disconnect)' #client initiated
     conn.close()
-    os._exit(0)
+    conn = None
+    print '[persistent] Connection socket closed'
+    
+def handle_client_nonpersistent(filename,conn):
+    f = open(filename,'rb')
+    line = f.read(1024)
+    counter = 0
+    while line:
+        conn.send(line)
+        conn.recv(10)
+        counter += 1
+        line = f.read(1024)
+    conn.send(b'end_of_file')
+    print "[nonpersistent] Sent {}, {}".format(counter,filename)
+    f.close()
+    conn.close() # server initiated
+    conn = None
+    print '[nonpersistent] Connection socket closed'
+    
 
 def time_now():
     return time.ctime(time.time())
 
 
 if __name__ == '__main__':
+    print "Connect to {}:{}".format(ADDR,PORT)
     main()
 
 
